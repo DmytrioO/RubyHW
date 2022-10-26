41ba0b261482a7d8c38bbadaa324fde42dcd78f9
Dmytro <odm200311@gmail.com>
2022-10-26 09:50:28 +0300

    Without 1 task

--- HW2/task1.rb
+++ HW2/task1.rb
@@ -1,19 +1,67 @@
 array = [621, 445, 147, 159, 430, 222, 482, 44, 194, 522, 652, 494, 14, 126, 532, 387, 441, 471, 337, 446, 18, 36, 202, 574, 556, 458, 16, 139, 222, 220, 107, 82, 264, 366, 501, 319, 314, 430, 55, 336]
 #Дізнатися кількість елементів в масиві
-puts array.length.to_s
+puts 'Дізнатися кількість елементів в масиві: ' + array.length.to_s
+puts ''
 #Перевернути масив
-puts array.reverse.to_s
+puts 'Перевернути масив: ' + array.reverse.to_s
+puts ''
 #Знайти найбільше число
-puts array.max.to_s
+puts 'Знайти найбільше число: ' + array.max.to_s
+puts ''
 #Знайти найменше число
-puts array.min.to_s
+puts 'Знайти найменше число: ' + array.min.to_s
+puts ''
 #Відсортувати від меншого до більшого
-puts array.sort.to_s
+puts 'Відсортувати від меншого до більшого: ' + array.sort.to_s
+puts ''
 #Відсортувати від більшого до меншого
-puts array.sort.reverse.to_s
+puts 'Відсортувати від більшого до меншого: ' + array.sort.reverse.to_s
+puts ''
 #Видалити всі непарні числа
-puts array.select { |e| e.even? }.to_s
+puts 'Видалити всі непарні числа: ' + array.select { |e| e.even? }.to_s
+puts ''
 #Залишити тільки ті числа, які без остачі діляться на 3
-puts array.select { |e| e % 3 == 0 }.to_s
+puts 'Залишити тільки ті числа, які без остачі діляться на 3: ' + array.select { |e| e % 3 == 0 }.to_s
+puts ''
 #Видалити з масиву числа, які повторюються
-puts array.uniq.to_s
+puts 'Видалити з масиву числа, які повторюються: ' + array.uniq.to_s
+puts ''
+#Розділити кожен елемент масиву на 10, при цьому елементи не мають бути округленими до 
+#цілого
+i = 0
+result = []
+array.each { |e| result[i] = e.to_f / 10; i = i + 1}
+puts 'Розділити кожен елемент масиву на 10, при цьому елементи не мають бути округленими до цілого: ' + result.to_s
+puts ''
+#Змінити місцями мінімальний і максимльний елементи
+array1 = array
+min_index = array1.index(array1.min)
+max_index = array1.index(array1.max)
+min = array1.min
+max = array1.max
+array1[min_index] = max
+array1[max_index] = min
+puts 'Змінити місцями мінімальний і максимльний елементи: ' + array1.to_s
+puts ''
+#Знайти елементи, які знаходяться перед мінімальним елементом в масиві
+array1 = []
+min_index = array.index(array.min)
+i = 0
+while i < min_index
+  array1.push(array[i])
+  i = i + 1
+end
+puts 'Знайти елементи, які знаходяться перед мінімальним елементом в масиві: ' + array1.to_s
+puts ''
+#Знайти три найменших елементи
+array1 = array
+max = array1.max
+i = 0
+min = []
+while i < 3
+	min.push(array1.min)
+	array1[array1.index(array1.min)] = max
+	i = i + 1
+end
+puts 'Знайти три найменших елементи: ' + min.to_s
+puts ''
