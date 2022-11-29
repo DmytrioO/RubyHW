9bf087e9bff16e6a85a7b49d471a9ed1e8a47daa
Dmytro <odm200311@gmail.com>
2022-11-29 21:03:14 +0200

    Changed my HW5

--- HW5/homework-api/app/controllers/api/v1/articles_controller.rb
+++ HW5/homework-api/app/controllers/api/v1/articles_controller.rb
@@ -1,11 +1,21 @@
 class Api::V1::ArticlesController < ApplicationController
+  before_action :find_article, only: [:update, :destroy]
+
   def index
     articles = Article.all
 
     if articles
-      render json: {status: 'SUCCESS', message: 'Fetched all the articles successfully', data: articles}, status: :ok
+      render json: { status: 'SUCCESS', message: 'Fetched all the articles successfully', data: articles }, status: :ok
+    end
+  end
+
+  def show
+    article = Article.find_by(id: params[:id])
+
+    if article
+      render json: { data: article }, status: :ok
     else
-      render json: articles.errors, status: :bad_request
+      render json: { status: 'NOT FOUND', message: 'Article could not be found' }, status: :bad_request
     end
   end
 
@@ -13,39 +23,29 @@ class Api::V1::ArticlesController < ApplicationController
     article = Article.new(article_params)
 
     if article.save
-      render json: {status: 'CREATED', message: 'Article was created successfully!', data: article}, status: :created
+      render json: { status: 'CREATED', message: 'Article was created successfully!', data: article }, status: :created
     else
       render json: article.errors, status: :unprocessable_entity
-    end
-  end
-
-  def show
-    article = Article.find(params[:id])
-
-    if article
-      render json: {data: article}, status: :ok
-    else
-      render json: {status: 'NOT FOUND', message: 'Article could not be found'}, status: :bad_request
-    end
-  end
-
-  def destroy
-    article = Article.find(params[:id])
-
-    if article.destroy!
-      render json: {status: 'SUCCESS', message: "Article was deleted successfully!"}, status: :ok
-    else
-      render json: {status: 'NOT FOUND', message: 'Article could not be found'}, status: :bad_request
     end
   end
 
   def update
     article = Article.find(params[:id])
 
-    if article.update!(article_params)
-      render json: {status: 'SUCCESS', message: "Article was updated successfully", data: article}, status: :ok
+    if article.update(article_params)
+      render json: { status: 'SUCCESS', message: "Article was updated successfully", data: article }, status: :ok
     else
-      render json: {message: "Article cannot be updated"}, status: :unprocessable_entity
+      render json: { message: "Article cannot be updated" }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    article = Article.find(params[:id])
+
+    if article.destroy
+      render json: { status: 'SUCCESS', message: "Article was deleted successfully!" }, status: :ok
+    else
+      render json: { status: 'NOT FOUND', message: 'Article could not be found' }, status: :bad_request
     end
   end
 
@@ -53,5 +53,9 @@ class Api::V1::ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :body)
+  end
+
+  def find_article
+    article = Article.find(params[:id])
   end
 end
