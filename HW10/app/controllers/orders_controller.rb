c64e4bf3b83ae10e45306fc93027eff594a76965
Dmytro <odm200311@gmail.com>
2023-01-26 13:53:45 +0200

    Set mailer with letter_opener

--- HW10/app/controllers/orders_controller.rb
+++ HW10/app/controllers/orders_controller.rb
@@ -17,7 +17,7 @@ class OrdersController < ApplicationController
     @order = Order.create(cart_id: cookies[:cart_id], user_id: current_user.id, payment_status: params[:pay])
     @order_info = OrderInformation.create(order_id: @order.id, total: cookies[:total])
     @order_info.update(order_params)
-    # OrderMailer.with(user: User.find(@order.user_id), order: @order).order_email.deliver_now
+    OrderMailer.order_email(@order.id).deliver_now
     cookies.delete :cart_id
     cookies.delete :total
   end
--- HW10/app/mailers/order_mailer.rb
+++ HW10/app/mailers/order_mailer.rb
@@ -4,6 +4,9 @@ class OrderMailer < ApplicationMailer
   def order_email(order_id)
     @order = Order.find(order_id)
     @user = @order.user
-    mail(to: @user.email, subject: 'Thank you fot your order!')
+    @order.cart.cart_products.each do |cart_product|
+      attachments["#{cart_product.name}.jpg"] = cart_product.product.image.download
+    end
+    mail(to: @user.email, subject: 'Thank you for your order!')
   end
 end
